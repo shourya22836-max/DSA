1111de74716c20a23c84da71a9b7b761b5bb23c2
shourya22836-max <shourya22836@gmail.com>
2026-09-19 23:08:39 +0530

    hashing

---
+++ basics_cpp/hashing.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+
+        long long sum = 0;
+        int left = 0;
+        int ans = 1;
+
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+
+            // Cost to make every element in the window equal to nums[right]
+            while (1LL * nums[right] * (right - left + 1) - sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
